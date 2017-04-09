0345351fac6664b768036a87c3e504a9729b5816
Johannes Trum <johannes@die-trums.de>
2017-04-09 20:36:10 +0200

    fix(msl): Correct transformation of subtitle entries in dash manifest

--- resources/lib/MSL.py
+++ resources/lib/MSL.py
@@ -355,25 +355,20 @@ class MSL:
                 segment_base = ET.SubElement(rep, 'SegmentBase', indexRange="0-"+str(init_length), indexRangeExact="true")
                 ET.SubElement(segment_base, 'Initialization', range='0-'+str(init_length))
 
-        # Multiple Adaption Set for subtiles
+        # Multiple Adaption Sets for subtiles
         for text_track in manifest['textTracks']:
-            print text_track
             if 'downloadables' not in text_track or text_track['downloadables'] is None:
                 continue
             subtiles_adaption_set = ET.SubElement(period, 'AdaptationSet',
                                                   lang=text_track['bcp47'],
+                                                  codecs='stpp',
                                                   contentType='text',
-                                                  mimeType='text/ttml')
+                                                  mimeType='application/ttml+xml')
             for downloadable in text_track['downloadables']:
                 rep = ET.SubElement(subtiles_adaption_set, 'Representation',
-                                    bandwidth='0',
                                     nflxProfile=downloadable['contentProfile']
                                     )
-                print downloadable['urls']
                 ET.SubElement(rep, 'BaseURL').text = self.__get_base_url(downloadable['urls'])
-
-
-
 
 
         xml = ET.tostring(root, encoding='utf-8', method='xml')
