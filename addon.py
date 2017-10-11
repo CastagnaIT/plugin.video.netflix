93b31ea0ba6d92cdaa2eed79bcab174350b762cf
asciidisco <public@asciidisco.com>
2017-10-11 15:27:51 +0200

    style(addon): Fixes linting issues

--- addon.py
+++ addon.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Author: asciidisco
 # Module: default
 # Created on: 13.01.2017
+# License: MIT https://goo.gl/5bMj3H
+
+"""Kodi plugin for Netflix (https://netflix.com)"""
+
 
 import sys
 from resources.lib.KodiHelper import KodiHelper
@@ -9,29 +13,30 @@ from resources.lib.Navigation import Navigation
 from resources.lib.Library import Library
 
 # Setup plugin
-plugin_handle = int(sys.argv[1])
-base_url = sys.argv[0]
+PLUGIN_HANDLE = int(sys.argv[1])
+BASE_URL = sys.argv[0]
+# We use string slicing to trim the leading ? from the plugin call paramstring
+REQUEST_PARAMS = sys.argv[2][1:]
 
 # init plugin libs
-kodi_helper = KodiHelper(
-    plugin_handle=plugin_handle,
-    base_url=base_url
+KODI_HELPER = KodiHelper(
+    plugin_handle=PLUGIN_HANDLE,
+    base_url=BASE_URL
 )
-library = Library(
-    root_folder=kodi_helper.base_data_path,
-    library_settings=kodi_helper.get_custom_library_settings(),
-    log_fn=kodi_helper.log
+LIBRARY = Library(
+    root_folder=KODI_HELPER.base_data_path,
+    library_settings=KODI_HELPER.get_custom_library_settings(),
+    log_fn=KODI_HELPER.log
 )
-navigation = Navigation(
-    kodi_helper=kodi_helper,
-    library=library,
-    base_url=base_url,
-    log_fn=kodi_helper.log
+NAVIGATION = Navigation(
+    kodi_helper=KODI_HELPER,
+    library=LIBRARY,
+    base_url=BASE_URL,
+    log_fn=KODI_HELPER.log
 )
-kodi_helper.set_library(library=library)
+KODI_HELPER.set_library(library=LIBRARY)
 
 if __name__ == '__main__':
     # Call the router function and pass the plugin call parameters to it.
-    # We use string slicing to trim the leading '?' from the plugin call paramstring
-    kodi_helper.log('Started (Version ' + kodi_helper.version + ')')
-    navigation.router(paramstring=sys.argv[2][1:])
+    KODI_HELPER.log('Started (Version ' + KODI_HELPER.version + ')')
+    NAVIGATION.router(paramstring=REQUEST_PARAMS)
