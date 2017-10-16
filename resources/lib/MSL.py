3863b1d88a55ec70682c4649668eeeeabaea7a30
asciidisco <public@asciidisco.com>
2017-10-16 13:19:09 +0200

    chore(lint): Improves code style

--- resources/lib/MSL.py
+++ resources/lib/MSL.py
@@ -547,11 +547,12 @@ class MSL(object):
             f.write(data)
         return base64.standard_b64encode(out.getvalue())
 
-    def __generate_msl_header(self,
-                            is_handshake=False,
-                            is_key_request=False,
-                            compressionalgo='GZIP',
-                            encrypt=True):
+    def __generate_msl_header(
+            self,
+            is_handshake=False,
+            is_key_request=False,
+            compressionalgo='GZIP',
+            encrypt=True):
         """
         Function that generates a MSL header dict
         :return: The base64 encoded JSON String of the header
