44262f234b2be233f55f6cba6014bd17f7284064
CastagnaIT <gottardo.stefano.83@gmail.com>
2019-10-17 17:57:47 +0200

    Fixed missing identifier in video_list_sorted

--- resources/lib/navigation/directory.py
+++ resources/lib/navigation/directory.py
@@ -89,7 +89,7 @@ class DirectoryBuilder(object):
             mainmenu_data = menu_data['main_menu']
         if menu_data.get('request_context_name', None) and g.is_known_menu_context(pathitems[2]):
             listings.build_video_listing(
-                api.video_list_sorted(context_name=menu_data['request_context_name'],
+                api.video_list_sorted(menu_data['request_context_name'],
                                       perpetual_range_start=self.perpetual_range_start,
                                       menu_data=mainmenu_data),
                 menu_data, pathitems)
@@ -97,7 +97,7 @@ class DirectoryBuilder(object):
             # Dynamic IDs for common video lists
             list_id = None if pathitems[2] == 'None' else pathitems[2]
             listings.build_video_listing(
-                api.video_list_sorted(context_name=menu_data['request_context_name'],
+                api.video_list_sorted(menu_data['request_context_name'],
                                       context_id=list_id,
                                       perpetual_range_start=self.perpetual_range_start,
                                       menu_data=mainmenu_data),
