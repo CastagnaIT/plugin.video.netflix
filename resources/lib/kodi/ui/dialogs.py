e3132af726c381e6a42a3eb3ed427fb5ee16b09b
CastagnaIT <gottardo.stefano.83@gmail.com>
2019-10-21 11:06:27 +0200

    Exception message fix

--- resources/lib/kodi/ui/dialogs.py
+++ resources/lib/kodi/ui/dialogs.py
@@ -9,6 +9,11 @@ import xbmcgui
 from resources.lib.globals import g
 import resources.lib.common as common
 
+try:  # Python 2
+    unicode
+except NameError:  # Python 3
+    unicode = str  # pylint: disable=redefined-builtin
+
 
 def show_notification(msg, title='Netflix', time=3000):
     """Show a notification"""
@@ -113,6 +118,5 @@ def show_addon_error_info(exc):
                           msg=common.get_local_string(30131))
     else:
         show_error_info(title=common.get_local_string(30105),
-                        message=': '.join((exc.__class__.__name__,
-                                           exc.message)),
+                        message=': '.join((exc.__class__.__name__, unicode(exc))),
                         netflix_error=False)
