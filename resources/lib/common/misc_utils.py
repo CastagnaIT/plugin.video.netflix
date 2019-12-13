2320171a6d410b683fe6e0229f5b7bddfb864218
CastagnaIT <gottardo.stefano.83@gmail.com>
2019-12-13 17:21:06 +0100

    Updated user agent for windows

--- resources/lib/common/misc_utils.py
+++ resources/lib/common/misc_utils.py
@@ -109,7 +109,7 @@ def get_user_agent():
         return base.replace('%PL%', '(Macintosh; Intel Mac OS X 10_14_6)')
     # Windows
     if system == 'Windows':
-        return base.replace('%PL%', '(Windows NT 6.1; WOW64)')
+        return base.replace('%PL%', '(Windows NT 10; Win64; x64)')
     # ARM based Linux
     if platform.machine().startswith('arm'):
         # Last number is the platform version of Chrome OS
